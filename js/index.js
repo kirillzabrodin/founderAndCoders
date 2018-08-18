ef4f52ecb54d2389dd24a63e843b01bfbd6ea57c
Kirill <kirillvzabrodin@gmail.com>
2018-08-18 16:32:40 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function toggleInfo(){
+    if($('#information').is(':visible') === true){
+        $('#information').slideUp();
+        $('#infoButton').html('Show info');
+    }else{
+        $('#information').slideDown();
+        $('#infoButton').html('Hide info');
+    }
+}
+
+function randomGrumpiness(){
+    var level = Math.random()*100;
+    $('#grumpinessMeter').attr('value',level);
+}
